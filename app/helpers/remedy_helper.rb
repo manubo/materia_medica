aa743a2ef71d93602f2ce73419ea12ab03487128
Manuel Bouza <manuel@bouza.ch>
2020-03-08 13:15:51 +0100

    Remove Kein Eintrag

--- app/helpers/remedy_helper.rb
+++ app/helpers/remedy_helper.rb
@@ -2,8 +2,6 @@ module RemedyHelper
   def entry_description(entry)
     if entry.description.present?
       simple_format(entry.description)
-    else
-      content_tag(:span, "Kein Eintrag", class: "text-secondary")
     end
   end
 end
