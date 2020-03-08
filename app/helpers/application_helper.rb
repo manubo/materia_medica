918ad4334112613d392fecc4291ae3fa465856a1
Manuel Bouza <manuel@bouza.ch>
2020-03-08 09:40:10 +0100

    Update nav actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,10 +6,9 @@ module ApplicationHelper
   def render_nav_actions
     content_tag(:div, class: "b") do
       if @remedy && request.path == remedy_path(@remedy)
-        link_to("Bearbeiten", edit_remedy_path(@remedy), class: "btn btn-sm btn-outline-primary btn-light", role: "button") +
-        link_to("Neues Mittel", new_remedy_path, class: "btn btn-sm btn-outline-secondary btn-light", role: "button")
-      else
-        link_to("Neues Mittel", new_remedy_path, class: "btn btn-sm btn-outline-primary btn-light", role: "button")
+        link_to("Bearbeiten", edit_remedy_path(@remedy), class: "btn btn-sm btn-outline-primary btn-light", role: "button")
+      elsif @remedy && request.path == edit_remedy_path(@remedy)
+        link_to("Abbrechen", remedy_path(@remedy), class: "btn btn-sm btn-outline-secondary btn-light", role: "button")
       end
     end
   end
