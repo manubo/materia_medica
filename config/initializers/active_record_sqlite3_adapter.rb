61d1cf0772d73e1cce5ebbc1d7cf3bf304b8d308
Manuel Bouza <manuel@bouza.ch>
2025-12-19 21:38:21 +0100

    Add support for PRAGMA configuration

---
+++ config/initializers/active_record_sqlite3_adapter.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module RailsExt
+  # Module to enhance SQLite3Adapter with pragma configuration support
+  module SQLite3Adapter
+    def configure_connection
+      super
+
+      @config[:pragmas].each do |key, value|
+        execute("PRAGMA #{key} = #{value}", 'SCHEMA')
+      end
+    end
+  end
+
+ActiveSupport.on_load(:active_record_sqlite3adapter) do
+  # self refers to `SQLite3Adapter` here,
+  # so we can call .prepend
+  prepend RailsExt::SQLite3Adapter
+end
